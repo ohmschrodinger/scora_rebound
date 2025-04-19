915b94eac26e9024ecee37b347d3d1cdf6bd36e8
mispranami <pranamiswarajindia@gmail.com>
2025-04-19 13:10:59 +0530

    first commit

---
+++ src/db/DBConnection.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package db;
+import java.sql.*;
+
+public class DBConnection {
+  private static Connection conn;
+   public static Connection getConnection() throws ClassNotFoundException,SQLException
+   {
+       String url = "jdbc:mysql://localhost:3306/examination_system";
+       String user = "root";
+       String pass = "9936360068";
+       if (conn==null)
+       {
+           Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(url,user,pass);
+            
+            System.out.println("database connected successfully");
+            
+            
+       }
+       return conn;
+   }
+}
+
